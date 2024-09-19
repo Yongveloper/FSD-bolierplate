5f62bcaab06d2f5d66c7419a7475f8984b7d431e
용현준 <hjyong@rsupport.com>
2024-09-19 16:30:46 +0900

    test: 테스트 제목 한글화

--- src/pages/root/root.page.test.tsx
+++ src/pages/root/root.page.test.tsx
@@ -3,8 +3,8 @@ import { describe, expect, it } from 'vitest';
 
 import { RootPage } from './root.page';
 
-describe('RootPage', () => {
-  it('renders the correct text', () => {
+describe('<RootPage />', () => {
+  it('루트 페이지가 정상적으로 렌더링 되어야 함.', () => {
     render(<RootPage />);
 
     expect(screen.getByText('첫 페이지입니다.')).toBeInTheDocument();
