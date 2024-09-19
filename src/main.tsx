327771b488507049654a48f53037e46b1ad64a80
yongveloper <dev_yong@kakao.com>
2024-09-19 16:33:08 +0900

    style: import prettier

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import './app/styles/index.css';
 import { GlobalSuspenseBoundary } from './app/provider';
 import { router } from './app/router';
+import './app/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
